4e7b4cde50306d26fb4c5800854b0d1b85542d79
Park Chanwoo <dev.halo1005@gmail.com>
2021-10-26 23:44:46 +0900

    멀티쓰레드 프로그래밍 - Interlocked

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,59 +4,36 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    // 메모리 베리어
-    // A) 코드 재배치 억제
-    // B) 가시성
-
-    // 1) Full Memory Barrier (ASM MFENCE, C# Thread.MemoryBarrier) : Store/Load 둘다 막는다
-    // 2) Store Memory Barrier (ASM SFENCE) : Store만 막는다
-    // 3) Load Memory Barrier (ASM LFENCE) : Load만 막는다
-
     class Program
     {
-        static volatile int x = 0;
-        static volatile int y = 0;
-        static volatile int r1 = 0;
-        static volatile int r2 = 0;
+        static int number = 0;
 
         static void Thread_1()
         {
-            y = 1; // Store y
-
-            Thread.MemoryBarrier();
-
-            r1 = x; // Load x
+            for (int i = 0; i < 1000000; ++i)
+            {
+                int afterValue = Interlocked.Increment(ref number);
+            }
         }
 
         static void Thread_2()
         {
-            x = 1; // Store x
-
-            Thread.MemoryBarrier();
-
-            r2 = y; // Load y
+            for (int i = 0; i < 1000000; ++i)
+            {
+                Interlocked.Decrement(ref number);
+            }
         }
 
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
-                x = y = r1 = r2 = 0;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
-                t1.Start();
-                t2.Start();
+            Task.WaitAll(t1, t2);
 
-                Task.WaitAll(t1, t2);
-
-                if (r1 == 0 && r2 == 0)
-                    break;
-            }
-
-            Console.WriteLine($"{count}번만에 빠져나옴!");
+            Console.WriteLine(number);
         }
     }
 }
