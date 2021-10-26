e800363a42ba0b7642e166009dfb8157c695ea16
Park Chanwoo <dev.halo1005@gmail.com>
2021-10-27 00:31:32 +0900

    멀티쓰레드 프로그래밍 - DeadLock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,6 +4,51 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class FastLock
+    {
+        public int id;
+    }
+
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+            }
+        }
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock (_lock)
+            {
+            }
+        }
+    }
+
     class Program
     {
         static int number = 0;
@@ -11,14 +56,9 @@ namespace ServerCore
 
         static void Thread_1()
         {
-            for (int i = 0; i < 1000000; ++i)
+            for (int i = 0; i < 100; ++i)
             {
-                // 상호배제 Mutual Exclusive
-
-                lock (obj)
-                {
-                    number++;
-                }
+                SessionManager.Test();
             }
         }
 
@@ -26,12 +66,9 @@ namespace ServerCore
 
         static void Thread_2()
         {
-            for (int i = 0; i < 1000000; ++i)
+            for (int i = 0; i < 100; ++i)
             {
-                lock (obj)
-                {
-                    number--;
-                }
+                UserManager.Test();
             }
         }
 
@@ -40,6 +77,9 @@ namespace ServerCore
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
+
+            Thread.Sleep(100);
+
             t2.Start();
 
             Task.WaitAll(t1, t2);
