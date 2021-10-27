3a92ff8e326dc0ef00338ad41e5e22c04fad6319
Park Chanwoo <dev.halo1005@gmail.com>
2021-10-28 00:30:03 +0900

    멀티쓰레드 프로그래밍 - ReaderWriterLock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,70 +4,40 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    //class Lock
-    //{
-    //    // bool <- 커널
-    //    AutoResetEvent available = new AutoResetEvent(true);
-    //    //ManualResetEvent available = new ManualResetEvent(true);
-
-    //    public void Acquire()
-    //    {
-    //        available.WaitOne(); // 입장 시도
-    //    }
-
-    //    public void Release()
-    //    {
-    //        available.Set(); // 문을 열어준다.
-    //    }
-    //}
-
     class Program
     {
-        static int num = 0;
-        //static Lock _lock = new Lock();
+        static object lock1 = new object();
+        static SpinLock lock2 = new SpinLock();
 
-        // int ThreadId
-        static Mutex _lock = new Mutex();
-
-        static void Thread_1()
+        class Reward
         {
-            for (int i = 0; i < 100000; ++i)
-            {
-                //_lock.Acquire();
-                //num++;
-                //_lock.Release();
 
-                _lock.WaitOne();
-                num++;
-                _lock.ReleaseMutex();
-            }
         }
 
-        static void Thread_2()
-        {
-            for (int i = 0; i < 100000; ++i)
-            {
-                //_lock.Acquire();
-                //num--;
-                //_lock.Release();
+        // RWLock ReaderWriterLock
+        static ReaderWriterLockSlim lock3 = new ReaderWriterLockSlim();
 
-                _lock.WaitOne();
-                num--;
-                _lock.ReleaseMutex();
-            }
+        static Reward GetRewardById(int id)
+        {
+            lock3.EnterReadLock();
+
+            lock3.ExitReadLock();
+
+            return null;
+        }
+
+        static void AddReward(Reward reward)
+        {
+            lock3.EnterWriteLock();
+
+            lock3.ExitWriteLock();
         }
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
-
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(num);
+            lock (lock1)
+            {
+            }
         }
     }
 }
