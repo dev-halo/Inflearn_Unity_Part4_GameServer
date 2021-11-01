9895867629327812f6d6ff36d118ece801dc823e
dev-halo <dev.halo1005@gmail.com>
2021-11-01 15:32:45 +0900

    유니티 연동 - 유니티 연동 #1

---
+++ Client/Assets/Scripts/Packet/PacketHandler.cs
@@ -0,0 +1,18 @@
+﻿using DummyClient;
+using ServerCore;
+using System;
+using UnityEngine;
+
+class PacketHandler
+{
+    public static void S_ChatHandler(PacketSession session, IPacket packet)
+    {
+        S_Chat chatPacket = packet as S_Chat;
+        ServerSession serverSession = session as ServerSession;
+
+        if (chatPacket.playerId == 1)
+            Debug.Log(chatPacket.chat);
+        //if (chatPacket.playerId == 1)
+            //Console.WriteLine(chatPacket.chat);
+    }
+}
--- DummyClient/Packet/ClientPacketManager.cs
+++ DummyClient/Packet/ClientPacketManager.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 class PacketManager
 {
     #region Singleton
-    static readonly PacketManager instance = new();
+    static readonly PacketManager instance = new PacketManager();
     public static PacketManager Instance { get { return instance; } }
     #endregion
 
@@ -14,8 +14,8 @@ class PacketManager
         Register();
     }
 
-    readonly Dictionary<ushort, Action<PacketSession, ArraySegment<byte>>> onRecv = new();
-    readonly Dictionary<ushort, Action<PacketSession, IPacket>> handler = new();
+    readonly Dictionary<ushort, Action<PacketSession, ArraySegment<byte>>> onRecv = new Dictionary<ushort, Action<PacketSession, ArraySegment<byte>>>();
+    readonly Dictionary<ushort, Action<PacketSession, IPacket>> handler = new Dictionary<ushort, Action<PacketSession, IPacket>>();
 
     public void Register()
     {
@@ -39,7 +39,7 @@ class PacketManager
 
     void MakePacket<T>(PacketSession session, ArraySegment<byte> buffer) where T : IPacket, new()
     {
-        T pkt = new();
+        T pkt = new T();
         pkt.Read(buffer);
         if (handler.TryGetValue(pkt.Protocol, out Action<PacketSession, IPacket> action))
             action.Invoke(session, pkt);
