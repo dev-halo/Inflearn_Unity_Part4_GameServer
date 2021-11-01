d48c61225157caf146c1286d24fc6fbc6be3b36a
dev-halo <dev.halo1005@gmail.com>
2021-11-01 11:59:02 +0900

    Job Queue - JobQueue #2

--- ServerCore/Listener.cs
+++ ServerCore/Listener.cs
@@ -9,7 +9,7 @@ namespace ServerCore
         Socket listenSocket;
         Func<Session> sessionFactory;
 
-        public void Init(IPEndPoint endPoint, Func<Session> sessionFactory)
+        public void Init(IPEndPoint endPoint, Func<Session> sessionFactory, int register = 10, int backlog = 100)
         {
             listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             this.sessionFactory += sessionFactory;
@@ -19,11 +19,14 @@ namespace ServerCore
 
             // 영업 시작
             // backlog : 최대 대기수
-            listenSocket.Listen(10);
+            listenSocket.Listen(backlog);
 
-            SocketAsyncEventArgs args = new();
-            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
-            RegisterAccept(args);
+            for (int i = 0; i < register; ++i)
+            {
+                SocketAsyncEventArgs args = new();
+                args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+                RegisterAccept(args);
+            }
         }
 
         void RegisterAccept(SocketAsyncEventArgs args)
